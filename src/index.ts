a537da63a0148cde59fe9088484c1cb665c66c4d
manishmh <manishmh982@gmail.com>
2025-05-12 01:42:51 +0530

    added maxAge to cors to prevent preflight

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000", "https://smart-hire-mh.vercel.app"],
-    credentials: true
+    credentials: true,
+    maxAge: 86400
 }));
 app.use(cookieParser());
 const limiter = rateLimit({
